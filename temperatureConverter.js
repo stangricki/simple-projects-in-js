827c86d57a088662053332ddcd3db87da53c66f1
Filip Stangricki <stangricki@gmail.com>
2017-11-28 22:44:29 +0100

    Improvement of Temperature Converter

--- temperatureConverter.js
+++ temperatureConverter.js
@@ -3,11 +3,15 @@
 // Ver 1.0
 // Made by Filip Stangricki
 
+function temperatureConverter() {
+	var getCelsius = prompt('Enter temperature in Celsius'); // Enter temperature here and run the code!
+	var getKelvin = Math.floor(getCelsius) + 273.15;
+	var getFahrenheit = getCelsius * 1.8 + 32;
 
-var getCelsius = 15; // Enter temperature here and run the code!
+	alert("Celsius " + getCelsius + '°C \n' + "Fahrenheit " + getFahrenheit  + '°F \n' + "Kelvin " + getKelvin + '°K \n');
+	if (confirm("Would you like to convert the temperature again?")) {
+		temperatureConverter();
+	} 
+};
 
-var getKelvin = Math.floor(getCelsius) + 273.15;
-var getFahrenheit = getCelsius * 1.8 + 32;
-
-console.log("Celsius " + getCelsius + '°C \n' + "Fahrenheit " + getFahrenheit  + '°F \n' + "Kelvin " + getKelvin + '°K \n');
-
+temperatureConverter();
