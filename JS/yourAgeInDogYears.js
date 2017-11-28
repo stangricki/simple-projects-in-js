b633e706c1de6cd67d996c2c6c71f3e2ff508b78
Filip Stangricki <stangricki@gmail.com>
2017-11-28 20:14:30 +0100

    Your age in dog years

---
+++ JS/yourAgeInDogYears.js
@@ -0,0 +1,11 @@
+let myAge = prompt('What\'s your age?');
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+let myAgeInDogYears = earlyYears + laterYears; 
+
+document.write('You are ' + myAgeInDogYears + ' years old in dog years.')
